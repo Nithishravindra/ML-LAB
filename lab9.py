adfd8690991de5d0dade21b2e84f88885c2e937a
nithish ravindra <nithishravindra8@gmail.com>
2021-01-26 22:47:57 +0530

    Update lab9.py

--- lab9.py
+++ lab9.py
@@ -5,19 +5,26 @@ from sklearn.model_selection import train_test_split
 
 iris_dataset = load_iris()
 print("Iris target names:", iris_dataset.target_names)
+
 for i in range(len(iris_dataset.target_names)):
     print(i, ":", iris_dataset.target_names[i])
+    
 print("iris data:", iris_dataset['data'])
+
 X_train, X_test, y_train, y_test = train_test_split(
     iris_dataset['data'], iris_dataset['target'], random_state=0)
 print("target:", iris_dataset['target'])
+
 kn = KNeighborsClassifier(n_neighbors=1)
 kn.fit(X_train, y_train)
+
 x_new = np.array([[5, 2.9, 1, .2]])
 print("XNEW", x_new)
+
 predicted = kn.predict(x_new)
 print("predicted target value:", predicted)
 print("predicted feature names:", iris_dataset['target_names'][predicted])
+
 i = 1
 x = X_test[i]
 x_new = np.array([x])
@@ -26,6 +33,7 @@ for i in range(len(X_test)):
     x = X_test[i]
 x_new = np.array([x])
 predicted = kn.predict(x_new)
+
 print("Actual:{0} {1}".format(
     y_test[i], iris_dataset['target_names'][y_test[i]]))
 print("Predicted:{0} {1}".format(
